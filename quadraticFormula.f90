30e64c16026f0af53ac17e0a21c9070141ee3854
Erik <erik@noreply@gmail.com>
2018-10-10 23:27:10 -0700

    v1.0

--- quadraticFormula.f90
+++ quadraticFormula.f90
@@ -3,30 +3,29 @@ PROGRAM Quadratic
 IMPLICIT NONE
 
 CHARACTER(len=12) :: arg 
-INTEGER :: i, d 
-INTEGER, DIMENSION(0:2) :: standardFormCoefficients
-COMPLEX, DIMENSION(2) :: roots
+INTEGER :: i
+REAL, DIMENSION(0:2) :: standardFormCoefficients
 CALL userInput(standardFormCoefficients)
-
-do d = 0, 2
-    WRITE(*,*) standardFormCoefficients(d)
-end do
+PRINT * , quadraticFormula(standardFormCoefficients(0),standardFormCoefficients(1),standardFormCoefficients(2))
 
 CONTAINS
- 
     SUBROUTINE userInput(standardFormCoefficients)
-        INTEGER,DIMENSION(0:2), intent(out) :: standardFormCoefficients ! Output variable
+        REAL,DIMENSION(0:2), intent(out) :: standardFormCoefficients ! Output variable
         do i = 1, 3
             CALL getarg(i, arg)
             READ(arg, *) standardFormCoefficients(i-1)
         END DO
     END SUBROUTINE userInput
-
-    COMPLEX FUNCTION quadraticFormula(roots, a,b,c)
-        INTEGER, intent(in) :: a,b,c
-        COMPLEX, DIMENSION(2) :: roots
-        !do i=0,5
-        !end do
+    FUNCTION quadraticFormula( a,b,c)
+        REAL, intent(in) :: a,b,c
+        COMPLEX, dimension(0:1) :: quadraticFormula
+        if(b**2-(4*a*c).ge.0) then
+            quadraticFormula(0) = CMPLX(((-1*b)+SQRT(b**2-(4*a*c)))/2*a, 0)
+            quadraticFormula(1) = CMPLX(((-1*b)-SQRT(b**2-(4*a*c)))/2*a, 0)
+        else
+            quadraticFormula(0) = CMPLX((-1*b)/2*a,(SQRT(-1*(b**2-(4*a*c))))/2*a)
+            quadraticFormula(1) = CMPLX((-1*b)/2*a,-1*(SQRT(-1*(b**2-(4*a*c))))/2*a)
+        end if
+        
     END FUNCTION quadraticFormula
-
 END PROGRAM Quadratic
